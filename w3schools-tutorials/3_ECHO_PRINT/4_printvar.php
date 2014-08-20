5ed28211a5e3167431788d2ee52949847d3d0f17
Kaustubh Joshi <thatsmekaustubh@gmail.com>
2014-08-20 13:57:55 +0000

    Advanced w3schools

---
+++ w3schools-tutorials/3_ECHO_PRINT/4_printvar.php
@@ -0,0 +1,10 @@
+<?php
+$txt1="Learn PHP";
+$txt2="W3Schools.com";
+$cars=array("Volvo","BMW","Toyota");
+
+print $txt1;
+print "<br>";
+print "Study PHP at $txt2";
+print "My car is a {$cars[0]}";
+?>
---
+++ w3schools-tutorials/4_Data_types/2_integers.php
@@ -0,0 +1,13 @@
+<?php 
+$x = 5985;
+var_dump($x);
+echo "<br>"; 
+$x = -345; // negative number 
+var_dump($x);
+echo "<br>"; 
+$x = 0x8C; // hexadecimal number
+var_dump($x);
+echo "<br>";
+$x = 047; // octal number
+var_dump($x);
+?>
